9bcc6f802b7358510ab9d622b8b0662c8f461c5d
hh-wu <58615099+hh-wu@users.noreply.github.com>
2021-02-03 23:36:31 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -9,9 +9,9 @@ build_dir = "../dist/"
 index_page_size = 10
 archives_page_size = 20
 template = {
-    "name": "Galileo",
+    "name": "Kepler",
     "type": "local",
-    "path": "../Galileo"
+    "path": "../Kepler"
 }
 enable_jsdelivr = {
     "enabled": False,
@@ -19,14 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "hh-wu的个人博客"
 site_logo = "${static_prefix}logo.png"
-site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+site_build_date = "2021-02-03"
+author = "hh-wu"
+email = "hi@.cn"
+author_homepage = "https://www.nick0x01.com"
+description = "不义而富且贵，于我如浮云"
+key_words = ['Maverick', '熊猫小A', 'kepler', 'blog']
 language = 'zh-CN'
 external_links = [
     {
